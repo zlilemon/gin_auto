d0e4ffd0609cf18c4e1af484f2b0e04e34bc0ac3
zlilemon <330478270@qq.com>
2023-06-05 20:46:02 +0800

    add main

---
+++ main.go
@@ -0,0 +1,32 @@
+package gin_auto
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	log.Init(log.NewOptions())
+	log.Info("log init success")
+
+	// 读取配置文件
+	config.InitConf()
+	log.Infof("mysql username:%s", config.MysqlOption.Username)
+
+	// 链接数据库实例
+	database.ConnectMysql()
+
+	// 初始化微信支付client
+	wxpay.InitWxPayClient()
+
+	router := router.InitRouter()
+
+	s := &http.Server{
+		Addr:           ":8090",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
